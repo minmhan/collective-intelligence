92e40b2a41d691a51e7ccd0dc0feeda80eb30d93
Min Min <minmhan@gmail.com>
2016-08-12 21:14:27 +0800

    add file

--- advancedclassify.py
+++ advancedclassify.py
@@ -6,90 +6,77 @@ Created on Wed Jul  6 11:06:11 2016
 """
 from pylab import *
 
+
 class matchrow:
-    def __init__(self,row,allnum=False):
+    def __init__(self, row, allnum=False):
         if allnum:
-            self.data=[float(row[i]) for i in range(len(row)-1)]
+            self.data = [float(row[i]) for i in range(len(row) - 1)]
         else:
-            self.data=row[0:len(row)-1]
-        self.match=int(row[len(row)-1])
-        
-    
-def loadmatch(f,allnum=False):
-    rows=[]
+            self.data = row[0:len(row) - 1]
+        self.match = int(row[len(row) - 1])
+
+
+def loadmatch(f, allnum=False):
+    rows = []
     with open(f) as file:
         for line in file:
-            rows.append(matchrow(line.split(','),allnum))
-        
+            rows.append(matchrow(line.split(','), allnum))
+
     return rows
-        
-        
+
+
 def plotagematches(rows):
-    xdm,ydm=[r.data[0] for r in rows if r.match==1], [r.data[1] for r in rows if r.match==1]        
-    xdn,ydn=[r.data[0] for r in rows if r.match==0], [r.data[1] for r in rows if r.match==0]
-    
-    plot(xdm,ydm,'go')
-    plot(xdn,ydn,'+')
+    xdm, ydm = [r.data[0] for r in rows if r.match == 1], [r.data[1] for r in rows if r.match == 1]
+    xdn, ydn = [r.data[0] for r in rows if r.match == 0], [r.data[1] for r in rows if r.match == 0]
+
+    plot(xdm, ydm, 'go')
+    plot(xdn, ydn, '+')
     show()
 
 
 def lineartrain(rows):
-    averages={}
-    counts={}
-    
+    averages = {}
+    counts = {}
+
     for row in rows:
         # Get the class of this point
-        cl=row.match
-        
-        averages.setdefault(cl,[0,0]*(len(row.data)))
-        counts.setdefault(cl,0)
-        
+        cl = row.match
+
+        averages.setdefault(cl, [0, 0] * (len(row.data)))
+        counts.setdefault(cl, 0)
+
         # Add this point to the averages
         for i in range(len(row.data)):
-            averages[cl][i]+=float(row.data[i])
-            
+            averages[cl][i] += float(row.data[i])
+
         # Keep track of how many points in each class
-        counts[cl]+=1
-        
+        counts[cl] += 1
+
     # Divide sums by counts to get the averages
-    for cl,avg in averages.items():
+    for cl, avg in averages.items():
         for i in range(len(avg)):
-            avg[i]/=counts[cl]
-            
+            avg[i] /= counts[cl]
+
     return averages
-    
-    
-def dotproduct(v1,v2):
-    return sum([v1[i]*v2[i] for i in range(len(v1))])    
-    
-    
-def dpclassify(point,avgs):
-    b=(dotproduct(avgs[1],avgs[1])-dotproduct(avgs[0],avgs[0]))/2
-    y=dotproduct(point,avgs[0])-dotproduct(point,avgs[1])+b
-    if y>0:
+
+
+def dotproduct(v1, v2):
+    return sum([v1[i] * v2[i] for i in range(len(v1))])
+
+
+def dpclassify(point, avgs):
+    b = (dotproduct(avgs[1], avgs[1]) - dotproduct(avgs[0], avgs[0])) / 2
+    y = dotproduct(point, avgs[0]) - dotproduct(point, avgs[1]) + b
+    if y > 0:
         return 0
     else:
         return 1
-        
-        
 
-agesonly = loadmatch('agesonly.csv',allnum=True)
-matchmaker=loadmatch('matchmaker.csv')
+
+agesonly = loadmatch('agesonly.csv', allnum=True)
+matchmaker = loadmatch('matchmaker.csv')
 
 avgs = lineartrain(agesonly)
 plotagematches(agesonly)
 print(avgs)
-print(dpclassify([30,25],avgs))
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(dpclassify([30, 25], avgs))
